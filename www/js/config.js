a85f7b7418e336f00ac9974bcb1a2e020c2adb29
guna1819 <37141457+guna1819@users.noreply.github.com>
2018-03-14 13:24:01 +0530

    Add files via upload

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://weborder.chennaiexpress.com/driver/api",
+	'DialogDefaultTitle':"chennai express driver app",	
+	'PushProjectID':"507008644414",	
+	'APIHasKey':""
+};
